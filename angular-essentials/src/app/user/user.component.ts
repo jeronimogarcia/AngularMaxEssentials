f322f4193c4703945a41f6f1920d4ebe2ca2c27c
jeronimogarcia <jero_garcia23@hotmail.com>
2025-03-17 14:04:28 +0100

    event listener - dynamic content

--- angular-essentials/src/app/user/user.component.ts
+++ angular-essentials/src/app/user/user.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../auxiliars/users/user';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const newRandomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[newRandomIndex];
+    console.log('Clicked!');
+  }
 }
