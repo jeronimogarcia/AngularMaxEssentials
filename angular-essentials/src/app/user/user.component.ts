f8bc062ce9e5ee37ec5be0d6fc14ab70e540e427
jeronimogarcia <jero_garcia23@hotmail.com>
2025-03-17 15:01:35 +0100

    using signal inputs

--- angular-essentials/src/app/user/user.component.ts
+++ angular-essentials/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../auxiliars/users/user';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  onSelectUser() {
-    const newRandomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[newRandomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {}
 }
